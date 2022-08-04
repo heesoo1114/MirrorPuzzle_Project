4f724ef84a29fc331a30f582b8f927801d4e590f
435yj <yujin050420@naver.com>
2022-08-04 18:08:58 +0900

    수정사항 병합

--- Assets/02. Scripts/Yujin/Libray/Interior/LibraryCloset.cs
+++ Assets/02. Scripts/Yujin/Libray/Interior/LibraryCloset.cs
@@ -4,5 +4,27 @@ using UnityEngine;
 
 public class LibraryCloset : InteractionObject
 {
+    int cnt = 0;
 
+    public override void InteractionEvent()
+    {
+        if(cnt <= 2)
+        {
+            transform.GetChild(cnt).gameObject.SetActive(false);
+
+            if(cnt == 0)
+                InventorySystem.Inst.AddItem("Library_A_Stamp");
+            else if (cnt == 1) 
+                InventorySystem.Inst.AddItem("Library_B_GoldBar");
+            else if (cnt == 2)
+                InventorySystem.Inst.AddItem("Library_C_Book");
+
+            Debug.Log(cnt);
+            cnt++;
+        }
+        else
+        {
+            base.InteractionEvent();
+        }
+    }
 }
