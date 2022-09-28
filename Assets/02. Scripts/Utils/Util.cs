e15b062b812b10b0f92eab1832110bcc84e405d9
zanke27 <zanke27@gmail.com>
2022-09-28 19:56:59 +0900

    풀링 만듬, 사운드 매니저 만듬 TestSound에 사용법 있음

---
+++ Assets/02. Scripts/Utils/Util.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Util
+{
+    public enum Bgm
+    {
+        main
+    }
+
+    public enum Effect
+    {
+        Move,
+        GameClear,
+        KeyAdd
+    }
+}
