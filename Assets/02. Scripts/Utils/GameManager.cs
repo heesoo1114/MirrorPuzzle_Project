74dcc320f122e39e272b15c960d783f00c96b35f
hyeyeon059 <leehuijuan0912@gmail.com>
2022-10-08 20:25:51 +0900

    플레이어 위치 저장기능 구현

--- Assets/02. Scripts/Utils/GameManager.cs
+++ Assets/02. Scripts/Utils/GameManager.cs
@@ -110,6 +110,10 @@ public class GameManager : MonoBehaviour
 
     public void GameLoad()
     {
+        if(!PlayerPrefs.HasKey("PlayerX"))
+        {
+            return;
+        }
         float x = PlayerPrefs.GetFloat("PlayerX");
         float y = PlayerPrefs.GetFloat("PlayerY");
 
