a8938223b2e3d1c4caf13b0ff08352fe9dddf07c
435yj <81200320+435yj@users.noreply.github.com>
2022-10-14 01:42:22 +0900

    타일맵 이동 가능, 클리어 아직x

--- Assets/02. Scripts/Yujin/BigBrother/TileClick.cs
+++ Assets/02. Scripts/Yujin/BigBrother/TileClick.cs
@@ -2,17 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.Events;
 using UnityEngine.EventSystems;
+using DG.Tweening;
+using Cinemachine.Utility;
 
 public class TileClick : MonoBehaviour, IPointerClickHandler
 {
     [SerializeField]
     private Image emptyEmage;
 
+    private Transform clickButtonTransform;
+
+    private void Start()
+    {
+        clickButtonTransform = GetComponent<Transform>();
+    }
+
     public void OnPointerClick(PointerEventData eventData)
     {
-        Debug.Log(eventData.position);
+        if(Vector2.Distance(emptyEmage.gameObject.transform.position, clickButtonTransform.position) <= 1.5f)
+        {
+            Vector2 changeTransform = emptyEmage.gameObject.transform.position;
 
+            emptyEmage.gameObject.transform.DOMove(clickButtonTransform.position, .1f); 
+            clickButtonTransform.DOMove(changeTransform, .1f);
+            //¼­·Î ¹Ù²ãÁÜ (swap)
+
+        }
     }
 }
+
+
