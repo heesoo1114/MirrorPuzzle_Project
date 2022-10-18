34a90c09414e051f7176fd3566569c7b4ee3abc7
minseo <cms20061112@gmail.com>
2022-10-18 23:26:45 +0900

    베란다 퍼즐 완성

--- Assets/02. Scripts/Minseo/SpriteChange.cs
+++ Assets/02. Scripts/Minseo/SpriteChange.cs
@@ -1,23 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpriteChange : MonoBehaviour
 {
-    SpriteRenderer flowerpot;
-    public SpriteRenderer ChangeFlowerpot;
+    public Sprite[] sprites = new Sprite[4];
+    public GameObject changeflower;
 
     void Start()
     {
-        flowerpot = GetComponent<SpriteRenderer>();
+
     }
         
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "LightCollider")
         {
-            flowerpot = ChangeFlowerpot;
-            Debug.Log(".");
+            StartCoroutine("ChangeSprite");
+        }
+    }
+
+    IEnumerator ChangeSprite()
+    {
+        for(int i = 0; i < 4; i++)
+        {
+            yield return new WaitForSeconds(1f);
+            changeflower.GetComponent<SpriteRenderer>().sprite = sprites[i];
         }
     }
 }
