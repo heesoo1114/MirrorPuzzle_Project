2c860ced7c19449c0bc99a696b6ba588f9f7f663
Dongyeop <kitty-777@naver.com>
2022-12-08 19:52:14 +0900

    컷씬 이름 색깔

--- Assets/02. Scripts/UI/TextPanal/NameTextPanal.cs
+++ Assets/02. Scripts/UI/TextPanal/NameTextPanal.cs
@@ -10,5 +10,11 @@ public class NameTextPanal : MonoBehaviour
     public void ChangeNameText(string name)
     {
         _nameText.text = name;
+        if (name == "루카스")
+            _nameText.color = new Color(0.7686274509803922f, 0.4666666666666667f, 0.3490196078431373f);
+        else if (name == "올리버")
+            _nameText.color = new Color(0, 0.6588235294117647f, 0);
+        else
+            _nameText.color = new Color(1, 1, 1);
     }
 }
